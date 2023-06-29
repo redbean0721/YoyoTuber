1ca7e5e231c912f3e7fa485c77c5012bd79f6a7a
redbean0721 <redbean0721@gmail.com>
2023-06-29 18:18:17 +0800

    建構基本專案

---
+++ YoyoTuber_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef YOYOTUBER_PRIVATE_H
+#define YOYOTUBER_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.0.1"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	""
+#define FILE_VERSION	"0.0.0.1"
+#define FILE_DESCRIPTION	"YoyoTuber可以用可愛的虛擬角色來模擬您對鍵盤、滑鼠等設備的操作"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"YoyoTuber"
+#define PRODUCT_VERSION	"0.0.0.1"
+
+#endif /*YOYOTUBER_PRIVATE_H*/
